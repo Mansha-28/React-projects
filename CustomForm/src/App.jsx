ee6bff1e2deed69110faf179782b118bea10eac4
mansha <nigwal.1998@gmail.com>
2024-02-27 14:09:37 +0530

    updating custom form

--- CustomForm/src/App.jsx
+++ CustomForm/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   const [label, setLabel] = useState('')
 
-  const [form, setForm] = useState(['mansha']);
+  const [form, setForm] = useState([]);
+
+  const [storedValues, setStoredValues] = useState([]);
 
   const values = []
   const [final, setFinal] = useState(false)
@@ -24,6 +26,8 @@ function App() {
       val
     }
     values.push(obj)
+
+    
   }
 
   const clickButton = (e) => {
@@ -31,57 +35,61 @@ function App() {
     console.log(label)
     setForm((prev)=>{return [...prev, label]})
     setLabel('')
+    setFinal(true)
   }
 
   const handleSave = (e) => {
     e.preventDefault()
-   console.log(values)
-   setFinal(true)
+    console.log(values)
+    const val = values.map((prev) => `${prev.key}: ${prev.val}`);
+    setStoredValues(val);
   }
 
   return (
-    <div>
-        <h1 className='text-3xl font-bold'>Custom Form</h1>
+    <div className='bg-gray-200 w-[50%] rounded shadow-md shadow-slate-400 items-center justify-center m-auto'>
+        <h1 className='text-3xl font-bold my-5 pt-5'>Custom Form</h1>
 
         <div>
           <form className="" action="" method="">
 
             <div className="flex flex-row items-center justify-center">
-              <div className="w-[100px]">
+              <div className="w-[200px]">
                 <label className="">Label Input</label>
               </div>
               <div className="">
-                <input type="text" value={label} onChange={clickHandler} name='label' className="border-black text-black border-2 m-2 p-2 rounded outline-none "/>
+                <input type="text" value={label} onChange={clickHandler} name='label' className="border-black min-w-60 text-black border-2 m-2 p-2 rounded outline-none "/>
               </div>
             </div>
-            <button  onClick={clickButton} disabled={!label.trim()} className={label.trim() ? 'border bg-green-700 p-2 rounded' : 'border bg-slate-400 p-2 rounded'}>Add Input</button>
+            <button  onClick={clickButton} disabled={!label.trim()} className={label.trim() ? 'border bg-green-700 p-2 my-4 rounded' : 'border bg-slate-400 my-4 p-2 rounded'}>Add Input</button>
 
 
             {
               form.map((item) => (
                 <div className='flex flex-row items-center justify-center'>
-                  <div className="w-[100px]">
-                    <label htmlFor="" className='text-center items-start justify-start'> {item}</label>
+                  <div className="w-[200px] bg-gray-100 p-2 rounded">
+                    <label htmlFor="" className='text-center  items-start justify-start'> {item}</label>
                   </div>
                   <div className="">
-                    <input type="text" name={item} onChange={handleValue} className="border-black text-black border-2 m-2 p-2 rounded outline-none"/>
+                    <input type="text" name={item} onChange={handleValue} className="border-black min-w-60 text-black border-2 m-2 p-2 rounded outline-none"/>
                   </div>
                 </div>
               ))
             }
 
-            <button className='bg-slate-400 rounded p-2 px-4' onClick={handleSave} >Save</button>
+           {
+             final && <button className='bg-slate-400 rounded p-2 px-4 my-4' onClick={handleSave} >Save</button>
+           }
 
             <hr />
 
-            {
-              final &&
-              values.map((item,index)=>(
-                <div className="">
-                  {item[index].key}
-                </div>
-              ))
-            }
+            <div>
+              <h2>{final ? 'Stored Values:' : ''}</h2>
+              <ul>
+                {storedValues.map((value, index) => (
+                  <li key={index}>{value}</li>
+                ))}
+              </ul>
+            </div>
           </form>
         </div>
     </div>
